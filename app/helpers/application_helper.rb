1672269d0c33e88c02d05ad2ca0df0a293a25208
Itsuki-S <itsuki.s625@gmail.com>
2020-08-30 15:27:35 +0000

    static_page controller added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = "Divers" # 自分のアプリ名
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
