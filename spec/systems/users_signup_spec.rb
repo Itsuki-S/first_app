7822f4fbae95cc796313c30d2778dacd24a089c6
Itsuki-S <itsuki.s625@gmail.com>
2020-09-13 07:21:49 +0000

    sign-up page was made

---
+++ spec/systems/users_signup_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe "UsersSignups", type: :system do
+
+  it "is invalid because it has no name" do
+    visit signup_path
+    fill_in 'Name', with: ''
+    fill_in 'Email', with: 'user@invalid'
+    fill_in 'Password', with: 'foo'
+    fill_in 'Password Confirmation', with: 'bar'
+    click_on 'Create my account'
+    expect(current_path).to eq signup_path
+    expect(page).to have_selector '#error_explanation'
+  end
+
+  it "is valid because it fulfils condition of input" do
+    visit signup_path
+    fill_in 'Name', with: 'Example User'
+    fill_in 'Email', with: 'user@example.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password Confirmation', with: 'password'
+    click_on 'Create my account'
+    # follow_redirect!
+    expect(current_path).to eq user_path(1)
+    expect(page).not_to have_selector '#error_explanation'
+  end
+end
